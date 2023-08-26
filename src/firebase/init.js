42ba5fb3a8f6cc1bea920aa6759a212c35bf2400
cosmos00232 <cosmos00232@gmail.com>
2023-08-26 23:21:51 +0900

    rm dev-dist // add firebase module

---
+++ src/firebase/init.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpF7seqSRoJsaJogh5Re08DK3aIISW9vA",
+  authDomain: "one-bible-pwa.firebaseapp.com",
+  databaseURL: "https://one-bible-pwa-default-rtdb.firebaseio.com",
+  projectId: "one-bible-pwa",
+  storageBucket: "one-bible-pwa.appspot.com",
+  messagingSenderId: "529759944358",
+  appId: "1:529759944358:web:3d66fd695a2da3ce4d3e67"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+export default firebase;
+
